d04ec5f7c29442582a691b26051e103d4f4c80a0
Matt G <mpgraham917@gmail.com>
2024-04-28 14:50:26 -0700

    Add instructional placeholder text to textarea

--- src/components/textarea/TextArea.tsx
+++ src/components/textarea/TextArea.tsx
@@ -3,6 +3,16 @@ import { useCallback, useEffect, useRef } from "react";
 import { activeTabIdAtom, textFamily } from "~/atoms";
 import "./TextArea.scss";
 
+const placeholder = `
+How to use:
+- Type anywhere below the tabs.
+- Click the tabs above to switch between notes.
+- Click the "+" icon (at the end of the tab list) to create a new note. 
+- Right click a tab and click "Delete" to delete a note.
+
+Note: All notes are stored in local storage (no cloud storage), so be careful what you save here.
+`.trim();
+
 export const TextArea = () => {
   const ref = useRef<HTMLTextAreaElement>(null);
   const activeTabId = useAtomValue(activeTabIdAtom);
@@ -26,6 +36,7 @@ export const TextArea = () => {
       className="main-text-area"
       value={text}
       onChange={handleTextChange}
+      placeholder={placeholder}
       autoFocus
     />
   );
