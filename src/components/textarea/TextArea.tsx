2c2c9516f62133ed20586721db1936a7724b67fe
Matt G <mpgraham917@gmail.com>
2024-07-21 19:53:19 -0700

    Update placeholder text

--- src/components/textarea/TextArea.tsx
+++ src/components/textarea/TextArea.tsx
@@ -3,15 +3,7 @@ import { useCallback, useEffect, useRef } from "react";
 import { activeTabIdAtom, textFamily } from "~/atoms";
 import "./TextArea.scss";
 
-const placeholder = `
-How to use:
-- Type anywhere below the tabs.
-- Click the tabs above to switch between notes.
-- Click the "+" icon (at the end of the tab list) to create a new note. 
-- Right click a tab and click "Delete" to delete a note.
-
-Note: All notes are stored in local storage (no cloud storage), so be careful what you save here.
-`.trim();
+const placeholder = `Type here...`.trim();
 
 export const TextArea = () => {
   const ref = useRef<HTMLTextAreaElement>(null);
